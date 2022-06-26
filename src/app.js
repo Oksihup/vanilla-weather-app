771e16fec91d8bf7d63a5e7b61754eed1a109de7
Oksihup <107550003+Oksihup@users.noreply.github.com>
2022-06-27 00:59:20 +0300

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperaturaElement = document.querySelector("#temperature");
+  temperaturaElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "df6e175e2124f7c88048e4cca8bb9e16";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
